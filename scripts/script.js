695dbc7ddf896dc0de58bbdc281fcd8ac478824a
Hasan Misbah <hasanmisbah01@gmail.com>
2020-09-09 01:29:20 +0600

    init Commit

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+let item = [{
+    name: 'Models',
+    submenu: [{
+        itemName: '718',
+        thumbnail: 'https://files.porsche.com/filestore/image/multimedia/none/carrange-flyout-718/thumbnail/f45e2ee9-2012-11ea-80c6-005056bbdc38/porsche-thumbnail.png',
+        thirdMenu: [{
+            menuName: '718 Caymans Model',
+            info: [{
+                image: '',
+                obj: [{
+                        power: ''
+                    },
+                    {
+                        mph: ''
+                    }, {
+                        MSRP: ''
+                    },
+
+                ],
+                buttons: [{
+                    name: "Compare",
+                    link: '#',
+                    class: 'has',
+                    icon: 'fas fa-home'
+                }],
+                footnote: '*Manufacturer’s Suggested Retail Price. Excludes options; taxes; title; registration; delivery, processing and handling fee; dealer charges. Dealer sets actual selling price.'
+            }, ]
+        }]
+    }]
+}]
