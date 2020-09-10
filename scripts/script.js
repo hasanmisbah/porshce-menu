bb9cd7dc1911df57a90d4853d0f3172e38b578f9
Hasan Misbah <hasanmisbah01@gmail.com>
2020-09-11 01:35:07 +0600

    Javascript menu initialized

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,18 @@
-let item = [{
-    name: 'Models',
-    submenu: [{
-        itemName: '718',
-        thumbnail: 'https://files.porsche.com/filestore/image/multimedia/none/carrange-flyout-718/thumbnail/f45e2ee9-2012-11ea-80c6-005056bbdc38/porsche-thumbnail.png',
-        thirdMenu: [{
-            menuName: '718 Caymans Model',
-            info: [{
-                image: '',
-                obj: [{
-                        power: ''
-                    },
-                    {
-                        mph: ''
-                    }, {
-                        MSRP: ''
-                    },
+$(function () {
 
-                ],
-                buttons: [{
-                    name: "Compare",
-                    link: '#',
-                    class: 'has',
-                    icon: 'fas fa-home'
-                }],
-                footnote: '*Manufacturer’s Suggested Retail Price. Excludes options; taxes; title; registration; delivery, processing and handling fee; dealer charges. Dealer sets actual selling price.'
-            }, ]
-        }]
-    }]
-}]
+    $('.menu-item a').mouseenter(function () {
+        $('.menu-models').addClass('active');
+        $('.menu-item-second-level').first().children('.navigation').addClass('active');
+    });
+
+    $('.menu-items-wrapper').mouseleave(function () {
+        $('.menu-models').removeClass('active');
+    });
+
+    $('.menu-item-second-level a.text').hover(function () {
+            $('.menu-item-second-level').children('.navigation').removeClass('active');
+            $(this).siblings('.navigation').addClass('active');
+        },
+
+    );
+});
