24f44d2615a566599ec88331fb9efefc0c6c5f4f
Hasan Misbah <hasanmisbah01@gmail.com>
2020-09-30 01:17:55 +0600

    Desktop Version Completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,50 @@
+$(function () {
     let dataModel = '';
-    $(function () {
-        $('.menu-item a.menu-text').hover(function () {
-            $('.menu-models').removeClass('active');
-            $(this).siblings('.menu-models').addClass('active');
-            $('.menu-item-second-level').first().children('.navigation').addClass('active');
-            $('.menu-item-second-level').first().children('a.text').addClass('active');
-            $('.navigation.active').find('a.menu-item-third-level-text').first().addClass('active');
-            if ($(this).parent().hasClass('has-no-third')) {
-                $(this).parent().children('.menu-models').find('.tech-data-container', '.tech-data').addClass('active');
-            };
-        });
+    $('.menu-item').hover(function (e) {
 
-        $('.menu-items-wrapper').mouseleave(function () {
-            $('.menu-models').removeClass('active');
-            $('.menu-item-second-level').first().children('a.text').removeClass('active');
-            $('.navigation.active').find('a.menu-item-third-level-text').first().removeClass('active');
-        });
+        //Remove if class exist
+        $('.menu-models').removeClass('active');
+        $('.menu-item a.menu-text').removeClass('active');
+        $('.navigation-model > .menu-item-second-level:first-child > .navigation > .navigation-menu > .menu-item-third-level:first-child > a.menu-item-third-level-text').removeClass('active');
+        $('.navigation-model > .menu-item-second-level:first-child > .tech-data-container > .tech-data:first-child').removeClass('active');
 
-        $('.menu-item-second-level a.text').hover(function () {
-            $('.menu-item-second-level').children('.navigation').removeClass('active');
-            $('.menu-item-second-level').first().children('a.text').removeClass('active');
-            $('.navigation.active').find('a.menu-item-third-level-text').first().removeClass('active');
-            $('.menu-item-second-level').first().children('a.text').removeClass('active');
-            $('.menu-item-second-level').children('a.text').removeClass('active');
-            $(this).siblings('.navigation').addClass('active');
-            $(this).siblings('.navigation').find('a.menu-item-third-level-text').first().addClass('active');
-        });
-
-        $('a.menu-item-third-level-text').hover(function (e) {
-            $('.menu-item-second-level a.text').removeClass('active');
-            $(this).parents('.menu-item-second-level').find('a.text').addClass('active');
-            dataModel = $(this).data().model;
-            $('a.menu-item-third-level-text').removeClass('active');
-            $(this).addClass('active');
-            $('.tech-data').removeClass('active');
-            $(`[data-id="${dataModel}"]`).addClass('active');
-        });
+        //Add class active
+        $(this).children('a.menu-text').addClass('active');
+        $(this).children('.menu-models').addClass('active');
+        $(this).find('.menu-item-second-level:first-child').children('a.text').addClass('active');
+        $(this).find('.menu-item-second-level:first-child').children('.navigation').addClass('active');
+        $('.navigation-model > .menu-item-second-level:first-child > .navigation > .navigation-menu > .menu-item-third-level:first-child', $(this))
+            .children('a.menu-item-third-level-text').addClass('active');
+        $('.navigation-model > .menu-item-second-level:first-child > .tech-data-container > .tech-data:first-child', $(this)).addClass('active');
 
     });
+    $('.menu-item-second-level').hover(function (e) {
+        if ($(this).closest('.menu-item').hasClass('has-no-third')) {
+            if ($(this).data().model) {
+                dataModel = $(this).data().model;
+                $('.tech-data').removeClass('active');
+                $('.tech-data[data-id=' + dataModel + ']').addClass('active');
+            }
+        }
+        $('.menu-item-second-level').children('a.text').removeClass('active');
+        $('.menu-item-second-level').children('.navigation').removeClass('active');
+        $(this).children('a.text').addClass('active');
+        $(this).children('.navigation').addClass('active');
+        $(this).children('.tech-data-container').children('.tech-data:first-child').addClass('active');
+        $(this).children('.navigation').children('.navigation-menu').children('.menu-item-third-level:first-child').children('a.menu-item-third-level-text').addClass('active');
+
+    });
+    $('.menu-item-third-level').hover(function (e) {
+        $('a.menu-item-third-level-text').removeClass('active');
+        $(this).children('a.menu-item-third-level-text').addClass('active');
+        if ($(this).children('a.menu-item-third-level-text').data().model) {
+            dataModel = $(this).children('a.menu-item-third-level-text').data().model;
+            $('.tech-data').removeClass('active');
+            $('.tech-data[data-id=' + dataModel + ']').addClass('active');
+        }
+    });
+
+    $('.menu-items-wrapper').mouseleave(function () {
+        $('.active').removeClass('active');
+    });
+});
